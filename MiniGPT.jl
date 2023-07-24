5b3bb2be83ae592716da40a9da7e636ad11e4dd0
nihilisticneuralnet <138315505+nihilisticneuralnet@users.noreply.github.com>
2023-07-24 18:46:42 +0530

    Update MiniGPT.jl

--- MiniGPT.jl
+++ MiniGPT.jl
@@ -1,2 +1,11 @@
 import Pkg
 Pkg.add("CUDA")
+
+using CUDA
+if CUDA.functional()
+    println("CUDA is functional.")
+    device_count = CUDA.device_count()
+    println("Number of CUDA devices: $device_count")
+else
+    println("CUDA is not functional. Please check your installation.")
+end
