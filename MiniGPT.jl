0a5b816c355b87f3d8f4a2bbc76f550998a36c6e
nihilisticneuralnet <138315505+nihilisticneuralnet@users.noreply.github.com>
2023-07-25 11:22:11 +0530

    Update MiniGPT.jl

--- MiniGPT.jl
+++ MiniGPT.jl
@@ -9,3 +9,16 @@ if CUDA.functional()
 else
     println("CUDA is not functional. Please check your installation.")
 end
+
+
+using Flux
+using Flux: @epochs, onehotbatch, mse, throttle
+using CUDA
+
+# hyperparameters
+batch_size = 64
+block_size = 256
+max_iters = 5000
+eval_interval = 500
+learning_rate = 3e-4
+device = cpu() 
